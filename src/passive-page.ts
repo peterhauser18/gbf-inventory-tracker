cf4136b63512e6b9e4eaf00457d621b8462d3044
peterhauser18 <peterhauser18@gmail.com>
2026-08-16 04:20:33 +0200

    Remove passive GBF page hook

--- src/passive-page.ts
+++
@@ -1,7 +0,0 @@
-import { installPassivePageObserver } from './passive/page-observer.ts';
-
-const SOURCE = 'gbfit-passive-account-v1';
-
-installPassivePageObserver(window, (response) => {
-  window.postMessage({ source: SOURCE, response }, window.location.origin);
-});
